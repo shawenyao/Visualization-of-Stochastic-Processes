fc09b7606121ae20c0e26335f6f63481a481fef8
Wenyao <38875149+shawenyao@users.noreply.github.com>
2018-05-09 11:11:11 -0700

    updated scale

--- main.R
+++ main.R
@@ -3,16 +3,16 @@ library(htmlwidgets)
 library(purrr)
 
 # Brownian Motion without drift
-bm1 <- plot_brownian_motion(drift = 0, volatility = 1, xlim = c(0, 10), ylim = c(-5, 5), step = 50)
+bm1 <- plot_brownian_motion(drift = 0, volatility = 1, xlim = c(0, 5), ylim = c(-5, 5), step = 50)
 
 # Brownian Motion with drift
-bm2 <- plot_brownian_motion(drift = 1, volatility = 1, xlim = c(0, 10), ylim = c(-5, 5), step = 50)
+bm2 <- plot_brownian_motion(drift = 1, volatility = 1, xlim = c(0, 5), ylim = c(-5, 5), step = 50)
 
 # Trend Normal without slope
-tn1 <- plot_trend_normal(slope = 0, intercept = 0, volatility = 1, xlim = c(-5, 5), ylim = c(-5, 5), step = 50)
+tn1 <- plot_trend_normal(slope = 0, intercept = 0, volatility = 1, xlim = c(0, 5), ylim = c(-5, 5), step = 50)
 
 # Trend Normal with slope
-tn2 <- plot_trend_normal(slope = 1, intercept = 1, volatility = 1, xlim = c(-5, 5), ylim = c(-5, 5), step = 50)
+tn2 <- plot_trend_normal(slope = 1, intercept = 0, volatility = 1, xlim = c(0, 5), ylim = c(-5, 5), step = 50)
 
 
 setwd("C:/Users/Wenyao/Desktop/R/Brownian_Motion_Visualization/webGL")
