a67a270aaef53fdd922605830ae8cc595270fa2d
Wenyao <38875149+shawenyao@users.noreply.github.com>
2018-05-09 10:23:38 -0700

    change size of rglwidget

--- main.R
+++ main.R
@@ -124,13 +124,13 @@ plot_trend_normal <- function(slope, intercept, volatility, xlim, ylim, step){
 
 
 
-bm1 <- plot_brownian_motion(drift = 0, volatility = 1, xlim = c(0, 10), ylim = c(-5, 5), step = 100)
+bm1 <- plot_brownian_motion(drift = 0, volatility = 1, xlim = c(0, 10), ylim = c(-5, 5), step = 50)
 
-bm2 <- plot_brownian_motion(drift = 1, volatility = 1, xlim = c(0, 10), ylim = c(-5, 5), step = 100)
+bm2 <- plot_brownian_motion(drift = 1, volatility = 1, xlim = c(0, 10), ylim = c(-5, 5), step = 50)
 
-tn1 <- plot_trend_normal(slope = 0, intercept = 0, volatility = 1, xlim = c(-5, 5), ylim = c(-5, 5), step = 100)
+tn1 <- plot_trend_normal(slope = 0, intercept = 0, volatility = 1, xlim = c(-5, 5), ylim = c(-5, 5), step = 50)
 
-tn2 <- plot_trend_normal(slope = 1, intercept = 1, volatility = 1, xlim = c(-5, 5), ylim = c(-5, 5), step = 100)
+tn2 <- plot_trend_normal(slope = 1, intercept = 1, volatility = 1, xlim = c(-5, 5), ylim = c(-5, 5), step = 50)
 
 
 setwd("C:/Users/Wenyao/Desktop/R/Brownian_Motion_Visualization/webGL")
