22765c16c3d8063b844e2a79e1ffc092312eb861
Wenyao <38875149+shawenyao@users.noreply.github.com>
2018-05-09 11:03:44 -0700

    comments update

--- main.R
+++ main.R
@@ -2,134 +2,16 @@ library(rgl)
 library(htmlwidgets)
 library(purrr)
 
-#' 
-#' @description 
-#' 
-#' @param xpoints
-#' @param ypoints
-#' @param zpoints
-#' 
-#' @return 
-#' 
-plot_pdf3d <- function(xpoints, ypoints, zpoints){
-  open3d()
-  
-  # plot the origin
-  plot3d(
-    x = 0, y = 0, z = 0,
-    xlim = range(xpoints), ylim = range(ypoints), zlim = c(0, 1),
-    size=5, col="red",
-    xlab="t", ylab="x", zlab="f(x)"
-  )
-  
-  # plot the probablity density functions
-  lapply(
-    1:length(xpoints),
-    function(i){
-      lines3d(
-        x = xpoints[i], 
-        y = ypoints, 
-        z = zpoints[i,],
-        lwd = 2, 
-        col = rainbow(length(xpoints))[i]
-      )
-    }
-  )
-  
-  # plot the highest points
-  lapply(
-    1:length(xpoints),
-    function(i){
-      plot3d(
-        x = xpoints[i], 
-        y = ypoints[which.max(zpoints[i, ])], 
-        z = zpoints[i, which.max(zpoints[i, ])], 
-        size = 5, 
-        col = rainbow(length(xpoints))[i], 
-        add = TRUE
-      )
-    }
-  )
-  
-  # view from a specific point
-  rgl.viewpoint(userMatrix = rotationMatrix(1.5, -0.5, 0.2, 0.4))
-  
-  return(scene3d())
-}
-
-
-#' @description 
-#' visulize the probabiltiy density function of Brownian motion
-#' 
-#' @param drift
-#' @param xlim
-#' @param step
-#' @param ylim
-#' @param ystep
-#' 
-#' @return 
-#' 
-plot_brownian_motion <- function(drift, volatility, xlim, ylim, step){
-  
-  xpoints <- seq(from = xlim[1], to = xlim[2], length = step)
-  ypoints <- seq(from = ylim[1], to = ylim[2], length = step)
-  zpoints <- matrix(
-    dnorm(
-      x = rep(ypoints, times = length(xpoints)),
-      mean = xpoints[1] + rep(xpoints, each = length(ypoints)) * drift,
-      sd = sqrt(rep(xpoints, each = length(ypoints)) - xpoints[1]) * volatility
-    ),
-    nrow = length(xpoints),
-    ncol = length(ypoints),
-    byrow = TRUE
-  )
-  
-  # disregard the deterministic case of Brownian motion at time 0
-  zpoints[1,] <- NA
-  
-  plot_pdf3d(xpoints = xpoints, ypoints = ypoints, zpoints = zpoints)
-}
-
-#' @description 
-#' visulize the probabiltiy density function of trend normal model
-#' 
-#' @param slope
-#' @param intercept
-#' @param volatility
-#' @param xlim
-#' @param step
-#' @param ylim
-#' @param ystep
-#' 
-#' @return 
-#' 
-plot_trend_normal <- function(slope, intercept, volatility, xlim, ylim, step){
-  
-  xpoints <- seq(from = xlim[1], to = xlim[2], length = step)
-  ypoints <- seq(from = ylim[1], to = ylim[2], length = step)
-  zpoints <- matrix(
-    dnorm(
-      x = rep(ypoints, times = length(xpoints)),
-      mean = slope * rep(xpoints, each = length(ypoints)) + intercept,
-      sd = volatility
-    ),
-    nrow = length(xpoints),
-    ncol = length(ypoints),
-    byrow = TRUE
-  )
-  
-  plot_pdf3d(xpoints = xpoints, ypoints = ypoints, zpoints = zpoints)
-}
-
-
-
-
+# Brownian Motion without drift
 bm1 <- plot_brownian_motion(drift = 0, volatility = 1, xlim = c(0, 10), ylim = c(-5, 5), step = 50)
 
+# Brownian Motion with drift
 bm2 <- plot_brownian_motion(drift = 1, volatility = 1, xlim = c(0, 10), ylim = c(-5, 5), step = 50)
 
+# Trend Normal without slope
 tn1 <- plot_trend_normal(slope = 0, intercept = 0, volatility = 1, xlim = c(-5, 5), ylim = c(-5, 5), step = 50)
 
+# Trend Normal with slope
 tn2 <- plot_trend_normal(slope = 1, intercept = 1, volatility = 1, xlim = c(-5, 5), ylim = c(-5, 5), step = 50)
 
 
@@ -153,6 +35,4 @@ list(
         )
     }
   ) 
-
-
 
